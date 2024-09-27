62855d87a538985110b853d7c40345b2f29a7928
Tejas Kulkarni <tejaskulkarni2801@gmail.com>
2024-09-27 22:13:31 +0530

    Commited:

---
+++ server/index.js
@@ -0,0 +1,64 @@
+const express = require("express")
+const app = express();
+
+
+const mongoose = require("mongoose")
+mongoose.connect('mongodb://localhost:27017/rateLimiter').then(()=>{
+    console.log("MongoDB connected!")
+}).catch(err=>{
+    console.log("Error while connecting the monodb", err)
+})
+
+
+const RateLimit = require('./models/RateLimit'); // this is a mongoose model
+const rateLimiter = async (req, res, next) => {
+    const userId = req.query.userId; // it retrives from the url 
+
+    // Check if userId is provided
+    if (!userId) {
+        return res.status(400).json({ message: 'userId is required' });
+    }
+
+    const timeWindow = 60 * 1000; // 1 min in milliseconds
+    const maxRequest = 5;
+
+    let user = await RateLimit.findOne({ userId });
+    if (!user) { // If user doesn't exist, create a new record
+        user = new RateLimit({
+            userId,
+            requestCount: 1,
+            firstRequest: Date.now()
+        });
+    } else {
+        const timeSinceFirstRequest = Date.now() - user.firstRequest;
+
+        if (timeSinceFirstRequest < timeWindow) {
+            if (user.requestCount >= maxRequest) {
+                return res.status(429).json({
+                    message: 'Too many requests. Try again later'
+                });
+            }else{
+                user.requestCount++;
+            }
+        } else {
+            // Resetting the count after the time window has passed
+            user.requestCount = 1;
+            user.firstRequest = Date.now();
+        }
+    }
+    await user.save(); // here we are waiting till the save method is executed
+    next();
+}
+
+app.get('/api/resource',rateLimiter, (req, res)=>{
+    res.send("A Quick Project in REST API => API Rate Limiter")
+})
+
+const nodePort = 3001
+app.listen(nodePort, (err)=>{
+    if(!err){
+        console.log("Node local server started at Port: 3001")
+    }else{
+        console.log("Some error while starting the Node local server!!!")
+    }
+})
